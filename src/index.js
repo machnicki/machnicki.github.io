c66dbade83e5435d234895a20be46f7ed903b685
Miron Machnicki <miron@machnicki.com.pl>
2016-04-09 18:51:36 +0100

    React init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('this is my first js command')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const dest = document.getElementById('content')
+
+ReactDOM.render(
+  <div>
+    Now this is my react page
+  </div>,
+  dest
+)
