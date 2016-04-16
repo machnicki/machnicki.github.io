a705a801041128fb813ed84bb7a803c14645491c
Miron Machnicki <miron@machnicki.com.pl>
2016-04-16 21:24:35 +0100

    Move to native modules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,11 @@
-import path from 'path'
+const path = require('path')
 
 const PATHS = {
   app: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build'),
 }
 
-export default {
+module.exports = {
   entry: {
     app: PATHS.app,
   },
