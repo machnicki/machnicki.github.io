70e25a6f239d3871bc114398cd5f6c73eb1ff65b
Miron Machnicki <miron@machnicki.com.pl>
2016-04-16 20:17:24 +0100

    Get react app running

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/app.jsx'
 
-const dest = document.getElementById('content')
-
-ReactDOM.render(
-  <div>
-    Now this is my react page
-  </div>,
-  dest
-)
+ReactDOM.render(<App />, document.getElementById('app'))
