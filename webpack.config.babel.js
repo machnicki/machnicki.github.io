01a88674e4c4401644f356dd756e4f9fb6a7ea94
Miron Machnicki <miron@machnicki.com.pl>
2016-04-16 18:05:36 +0100

    ESLint and JSCS support (airbnb style)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,5 +3,11 @@ import webpack from 'webpack'
 export default {
   entry: {
     'index.js': './src/index.js',
+  },
+  module: {
+    preLoaders: [
+      test: /\.(js|jsx)$/,
+      loaders: ['eslint', 'jscs'],
+    ]
   }
 }
