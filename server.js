d478f936f742f2b194660d415ea4453b6c2d91e4
Miron Machnicki <miron@machnicki.com.pl>
2016-09-16 18:39:03 +0100

    Start static server and API by one command

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const backend = require('./server/server.js').init
+
+const app = express();
 
 app.use(express.static('build'))
 
 app.listen(process.env.PORT || 5000, function() {
   console.log('App listenning on port', process.env.PORT || 5000);
 })
+
+backend()
--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const db = require ('./db')
-db.init();
 
 const mainRoute = require('./routes/index')
 const usersRoute = require('./routes/users')
@@ -15,18 +14,24 @@ const carsRoute = require('./routes/cars')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+module.exports = {
+  init() {
+    db.init();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
-  next()
-})
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', mainRoute)
-app.use('/users', usersRoute)
-app.use('/cars', carsRoute)
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*")
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+      res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+      next()
+    })
 
-app.listen(3000, () => { console.log('App is listenning on port 3000') })
+    app.use('/', mainRoute)
+    app.use('/users', usersRoute)
+    app.use('/cars', carsRoute)
+
+    app.listen(3000, () => { console.log('App is listenning on port 3000') })
+  }
+}
